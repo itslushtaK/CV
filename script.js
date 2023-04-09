326b984bbd0ad031730f066e949f4c4782317431
Gentuar Lushtaku <93317180+itslushtaK@users.noreply.github.com>
2023-04-09 11:00:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// Sticky Navigation Menu Js
+
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+let val;
+
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+
+// Side Navigation Menu Js
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function() {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflowX = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+
+cancelBtn.onclick = function() {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflowX = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We click On Navigation Links
+
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
+const showMoreButton = document.getElementById('show-more');
+const showLessButton = document.getElementById('show-less');
+const hiddenPhotos = document.getElementById('hidden-photos');
+
+showMoreButton.addEventListener('click', function() {
+  hiddenPhotos.style.display = 'block';
+  showMoreButton.style.display = 'none';
+  showLessButton.style.display = 'block';
+});
+
+showLessButton.addEventListener('click', function() {
+  hiddenPhotos.style.display = 'none';
+  showMoreButton.style.display = 'block';
+  showLessButton.style.display = 'none';
+});
+
+
+
